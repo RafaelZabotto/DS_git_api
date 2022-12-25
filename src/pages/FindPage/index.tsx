ab4d468108b84ab0fe7d9c74ec93592ae84698eb
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2022-12-25 17:06:21 -0300

    Desafio API Git

--- src/pages/FindPage/index.tsx
+++ src/pages/FindPage/index.tsx
@@ -1,31 +1,94 @@
 import ProfileImg from 'assets/images/profile-img.png';
-import Navbar from 'components/Navbar';
+import ResultCard from 'components/ResultCard';
+import axios from 'axios';
 import './styles.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+type FormData = {
+  profile: string;
+};
+
+type GitProfile = {
+  avatar_url: string;
+  url: string;
+  followers: string;
+  location: string;
+  name: string;
+};
 
 const FindPage = () => {
+  const [gitprofile, setGitProfile] = useState<GitProfile>();
+
+  const [formData, setFormData] = useState<FormData>({
+    profile: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios
+      .get(`https://api.github.com/users/${formData.profile}`)
+      .then((response) => {
+        setGitProfile(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setGitProfile(undefined);
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <div className="search-container">
-        <div className='search-content-container'>
+        <div className="search-content-container">
           <h2>Encontre um perfil Github</h2>
-          <input type="text" placeholder='Usuário Github' />
+          <form onSubmit={handleSubmit}>
+            <div className="form-container">
+              <input
+                type="text"
+                name="profile"
+                value={formData.profile}
+                className="search-input"
+                placeholder="Usuário Github"
+                onChange={handleChange}
+              />
+              <button type="submit" className="btn-find">
+                Encontrar
+              </button>
+            </div>
+          </form>
         </div>
-        <button className="btn-find">Encontrar</button>
       </div>
-      <div className='result-container'>
-        <div className='result-img-profile'>
-            <img src={ProfileImg} alt="foto profile"/>
+      {gitprofile &&
+      <div className="result-container">
+        <div className="result-img-profile">
+          <img src={gitprofile.avatar_url} alt="foto_profile" />
         </div>
-        <div className='result-information-profile'>
-            <h4>Informações</h4>
-            <ul>
-                <li>Perfil</li>
-                <li>Seguidores</li>
-                <li>Localidade</li>
-                <li>Nome</li>
-            </ul>
+        <div className="result-information-profile">
+          <h4>Informações</h4>
+          <ResultCard
+            title="Perfil:"
+            description={gitprofile.url}
+          />
+          <ResultCard title="Seguidores:" description={gitprofile.followers} />
+          <ResultCard title="Localidade:" description={gitprofile.location} />
+          <ResultCard title="Nome:" description={gitprofile.name} />
         </div>
       </div>
+      }
     </>
   );
 };
